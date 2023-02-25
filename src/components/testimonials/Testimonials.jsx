f167ce91d5a95854313dda2210dbfbf82ed3929d
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2023-02-25 20:03:17 +0900

    Testimonials Component: CSS done

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -14,54 +14,50 @@ const Testimonials = () => {
                 <article className="testimonial">
                     <div className="client__avatar">
                         <img src={AVTR1} alt="Avatar One" />
-                        <h5 className="client__name">Ernest Achiever</h5>
-                        <small className="client__review">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Iste enim eos nesciunt? Minima harum quo,
-                            reprehenderit sed ipsam maxime nulla nesciunt
-                            necessitatibus animi voluptatem accusantium odit
-                            nihil, commodi aliquam amet.
-                        </small>
                     </div>
+                    <h5 className="client__name">Ernest Achiever</h5>
+                    <small className="client__review">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Iste enim eos nesciunt? Minima harum quo, reprehenderit
+                        sed ipsam maxime nulla nesciunt necessitatibus animi
+                        voluptatem accusantium odit nihil, commodi aliquam amet.
+                    </small>
                 </article>
                 <article className="testimonial">
                     <div className="client__avatar">
                         <img src={AVTR1} alt="Avatar One" />
-                        <h5 className="client__name">Ernest Achiever</h5>
-                        <small className="client__review">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Iste enim eos nesciunt? Minima harum quo,
-                            reprehenderit sed ipsam maxime nulla nesciunt
-                            necessitatibus animi voluptatem accusantium odit
-                            nihil, commodi aliquam amet.
-                        </small>
                     </div>
+                    <h5 className="client__name">Ernest Achiever</h5>
+                    <small className="client__review">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Iste enim eos nesciunt? Minima harum quo, reprehenderit
+                        sed ipsam maxime nulla nesciunt necessitatibus animi
+                        voluptatem accusantium odit nihil, commodi aliquam amet.
+                    </small>
                 </article>
                 <article className="testimonial">
                     <div className="client__avatar">
                         <img src={AVTR1} alt="Avatar One" />
-                        <h5 className="client__name">Ernest Achiever</h5>
-                        <small className="client__review">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Iste enim eos nesciunt? Minima harum quo,
-                            reprehenderit sed ipsam maxime nulla nesciunt
-                            necessitatibus animi voluptatem accusantium odit
-                            nihil, commodi aliquam amet.
-                        </small>
                     </div>
+                    <h5 className="client__name">Ernest Achiever</h5>
+                    <small className="client__review">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Iste enim eos nesciunt? Minima harum quo, reprehenderit
+                        sed ipsam maxime nulla nesciunt necessitatibus animi
+                        voluptatem accusantium odit nihil, commodi aliquam amet.
+                    </small>
                 </article>
                 <article className="testimonial">
                     <div className="client__avatar">
                         <img src={AVTR1} alt="Avatar One" />
-                        <h5 className="client__name">Ernest Achiever</h5>
-                        <small className="client__review">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Iste enim eos nesciunt? Minima harum quo,
-                            reprehenderit sed ipsam maxime nulla nesciunt
-                            necessitatibus animi voluptatem accusantium odit
-                            nihil, commodi aliquam amet.
-                        </small>
                     </div>
+                    <h5 className="client__name">Ernest Achiever</h5>
+                    <small className="client__review">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Iste enim eos nesciunt? Minima harum quo, reprehenderit
+                        sed ipsam maxime nulla nesciunt necessitatibus animi
+                        voluptatem accusantium odit nihil, commodi aliquam amet.
+                    </small>
                 </article>
             </div>
         </section>
