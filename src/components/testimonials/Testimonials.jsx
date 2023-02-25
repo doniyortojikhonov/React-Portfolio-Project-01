e3ce2047c4539b32d9116ac5be84fb649273e86e
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2023-02-25 20:12:07 +0900

    Testimonials component: Done with media querries

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -25,7 +25,7 @@ const Testimonials = () => {
                 </article>
                 <article className="testimonial">
                     <div className="client__avatar">
-                        <img src={AVTR1} alt="Avatar One" />
+                        <img src={AVTR2} alt="Avatar One" />
                     </div>
                     <h5 className="client__name">Ernest Achiever</h5>
                     <small className="client__review">
@@ -37,7 +37,7 @@ const Testimonials = () => {
                 </article>
                 <article className="testimonial">
                     <div className="client__avatar">
-                        <img src={AVTR1} alt="Avatar One" />
+                        <img src={AVTR3} alt="Avatar One" />
                     </div>
                     <h5 className="client__name">Ernest Achiever</h5>
                     <small className="client__review">
@@ -49,7 +49,7 @@ const Testimonials = () => {
                 </article>
                 <article className="testimonial">
                     <div className="client__avatar">
-                        <img src={AVTR1} alt="Avatar One" />
+                        <img src={AVTR4} alt="Avatar One" />
                     </div>
                     <h5 className="client__name">Ernest Achiever</h5>
                     <small className="client__review">
