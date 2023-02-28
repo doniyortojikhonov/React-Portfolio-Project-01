9134eed29d77a1e5373fe7b90ce2a2a6771b8f4f
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2023-02-28 23:22:59 +0900

    Footer Component: JSX done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,58 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
     return (
-        <section id="footer">
-            <h1>footer</h1>
-        </section>
+        <footer id="footer">
+            <a href="#" className="footer__logo">
+                DONIK
+            </a>
+
+            <ul className="permalinks">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+                <li>
+                    <a href="#services">Services</a>
+                </li>
+                <li>
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#testimonials">Testimonials</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+
+            <div className="footer__socials">
+                <a target="_blank" href="https://facebook.com">
+                    <FaFacebookF />
+                </a>
+                <a target="_blank" href="https://github.com/doniyortojikhonov">
+                    <BsGithub />
+                </a>
+                <a
+                    target="_blank"
+                    href="https://www.linkedin.com/in/doniyor-tojikhonov-2ab5b3205/"
+                >
+                    <FaLinkedinIn />
+                </a>
+            </div>
+
+            <div className="footer__copyright">
+                <small>&copy; Doniyor Tojikhonov. All rights reserved.</small>
+            </div>
+        </footer>
     );
 };
 
